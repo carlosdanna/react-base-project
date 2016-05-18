d524aa4f67d7b39df465c56590d1c41fb72e5aae
Carlos Danna <carlos.danna@gmail.com>
2016-05-18 10:46:50 -0400

    change on the gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,16 @@ var gulp = require('gulp');
 var browserify = require('browserify');
 var reactify = require('reactify');
 var source = require('vinyl-source-stream');
+var browserSync = require('browser-sync').create();
 
 gulp.task('browserify', function(){
     browserify('./src/js/main.js')
         .transform('reactify')
         .bundle()
+            .on('error', function(err){
+                console.log(err.message);
+                this.emit('end');
+            })
         .pipe(source('main.js'))
         .pipe(gulp.dest('dist/js'));
 });
@@ -16,10 +21,18 @@ gulp.task('copy', function(){
         .pipe(gulp.dest('dist'));
     gulp.src('src/css/*.*')
         .pipe(gulp.dest('dist/css'));
+    gulp.src('src/css/**')
+        .pipe(gulp.dest('dist/css'));
     gulp.src('src/js/vendors/*.*')
         .pipe(gulp.dest('dist/js'));
 });
 
+
 gulp.task('default', ['browserify', 'copy'], function(){
-    return gulp.watch('src/**/*.*', ['browserify', 'copy']);
+    browserSync.init({
+        server: './dist'
+    });
+    gulp.watch('src/**/*.*', ['browserify', 'copy']);
+    gulp.watch('./dist/js/main.js', browserSync.reload);
+
 })
